d768a7927fe04244bf4e6f9b8d2ba8cef6ce84c5
Kanny <kanny.ghafour87@gmail.com>
2023-09-11 22:25:08 -0500

    lab completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, quest, setQuest }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,39 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const deletedQues = quest.filter((q) => q.id !== id);
+        setQuest(deletedQues);
+      });
+  }
+
+  function handleCorrectIndex(correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex : parseInt(correctIndex) }),
+    })
+      .then((r) => r.json())
+      .then((data) => console.log(data.formData));
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={(e)=> handleCorrectIndex(e.target.value)}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [quest, setQuest] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(res => res.json())
+    .then(data => setQuest(data))
+  }, [])
+
   return (
-    <section>
+    <>
+      <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+      <ul>
+        {quest.map((q) => (
+          <QuestionItem question={q} key={q.id} quest={quest} setQuest={setQuest} />
+        ))}
+      </ul>
+      </section>
+
+    </>
   );
 }
 
